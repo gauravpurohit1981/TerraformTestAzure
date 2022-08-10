dbf2eecd65e7dd5dc36f810a7049f102de0d0d97
gauravpurohit1981 <43321789+gauravpurohit1981@users.noreply.github.com>
2022-08-10 14:45:23 +0100

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -1,6 +1,6 @@
 backend "azurerm" {
-    resource_group_name   = "sanuj-infra"
-    storage_account_name  = "sanujtstate"
+    resource_group_name   = "sunaj-infra"
+    storage_account_name  = "sunajtstate"
     container_name        = "tstate"
     key                   = "77Q4LUB5o9wRdbPYDt+0kGZP+L8Sj9E/FNXg7lZBQS5z3mLod5cyan4wA19CR1SmlqIRUFQfhuQrPVaGzNhjGw=="
 }
