ccfba27c46bde8519a43746ab37a7efb68454743
gauravpurohit1981 <43321789+gauravpurohit1981@users.noreply.github.com>
2022-08-10 14:10:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "jonnychipz-infra"
-    storage_account_name = "jonnychipztstate"
+    resource_group_name  = "sunaj-infra"
+    storage_account_name = "sunajstate"
     container_name       = "tstate"
     key                  = "77Q4LUB5o9wRdbPYDt+0kGZP+L8Sj9E/FNXg7lZBQS5z3mLod5cyan4wA19CR1SmlqIRUFQfhuQrPVaGzNhjGw=="
   }
@@ -24,10 +24,10 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 # Create our Resource Group - Jonnychipz-RG
 resource "azurerm_resource_group" "rg" {
-  name     = "jonnychipz-app01"
+  name     = "sunaj-app01"
   location = "UK South"
 }
-# Create our Virtual Network - Jonnychipz-VNET
+# Create our Virtual Network -VNET
 resource "azurerm_virtual_network" "vnet" {
   name                = "jonnychipzvnet"
   address_space       = ["10.0.0.0/16"]
@@ -41,20 +41,20 @@ resource "azurerm_subnet" "sn" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-# Create our Azure Storage Account - jonnychipzsa
-resource "azurerm_storage_account" "jonnychipzsa" {
-  name                     = "jonnychipzsa"
+# Create our Azure Storage Account
+resource "azurerm_storage_account" "sunajsa" {
+  name                     = "sunajsa"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   tags = {
-    environment = "jonnychipzenv1"
+    environment = "sunaj"
   }
 }
 # Create our vNIC for our VM and assign it to our Virtual Machines Subnet
 resource "azurerm_network_interface" "vmnic" {
-  name                = "jonnychipzvm01nic"
+  name                = "sunajvm01nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -64,9 +64,9 @@ resource "azurerm_network_interface" "vmnic" {
     private_ip_address_allocation = "Dynamic"
   }
 }
-# Create our Virtual Machine - Jonnychipz-VM01
-resource "azurerm_virtual_machine" "jonnychipzvm01" {
-  name                  = "jonnychipzvm01"
+# Create our Virtual Machine 
+resource "azurerm_virtual_machine" "sunajwebservervm01" {
+  name                  = "sunajwebservervm01"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.vmnic.id]
@@ -74,19 +74,19 @@ resource "azurerm_virtual_machine" "jonnychipzvm01" {
   storage_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2016-Datacenter-Server-Core-smalldisk"
+    sku       = "2019-Datacenter-Server-Core-smalldisk"
     version   = "latest"
   }
   storage_os_disk {
-    name              = "jonnychipzvm01os"
+    name              = "webservervm01os"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
-    computer_name  = "jonnychipzvm01"
-    admin_username = "jonnychipz"
-    admin_password = "Password123$"
+    computer_name  = "sujanwebservervm01"
+    admin_username = ""
+    admin_password = ""
   }
   os_profile_windows_config {
   }
